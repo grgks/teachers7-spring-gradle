8242a361420d7d5ee40207bf7da41b563b445b0b
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-05-29 22:50:48 +0300

    add Exceptions

--- src/main/java/gr/aueb/cf/teacherapp/core/exceptions/EntityGenericException.java
+++ src/main/java/gr/aueb/cf/teacherapp/core/exceptions/EntityGenericException.java
@@ -1,7 +1,10 @@
 package gr.aueb.cf.teacherapp.core.exceptions;
 
-public class EntityGenericException extends RuntimeException {
-    public EntityGenericException(String message) {
+public class EntityGenericException extends Exception {
+    private final String code;
+
+    public EntityGenericException(String code, String message) {
         super(message);
+        this.code = code;
     }
 }
