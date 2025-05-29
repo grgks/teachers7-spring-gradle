00a3c3540c2f9c819b0b6d0bcb945e0cdbe4eb3f
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-05-29 22:39:59 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/teacherapp/core/exceptions/EntityGenericException.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.teacherapp.core.exceptions;
+
+public class EntityGenericException extends RuntimeException {
+    public EntityGenericException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/gr/aueb/cf/teacherapp/mapper/Mapper.java
@@ -0,0 +1,4 @@
+package gr.aueb.cf.teacherapp.mapper;
+
+public class Mapper {
+}
